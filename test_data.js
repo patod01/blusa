e4d898bdef9bf107901e7dc2272687a958c6eee1
patod01 <patocv2@hotmail.com>
2024-04-28 16:24:33 -0400

    Adds test data. Needs to be imported

---
+++ test_data.js
@@ -0,0 +1,17 @@
+dev = true;
+
+data = [
+     ['19-04-2024', 271637, 230757, 'juegos',      'generico 1',  'corriente'],
+     ['13-04-2024', 47749,  474781, 'actividades', 'generico 2',  'credito 2'],
+     ['07-04-2024', 625238, 187245, 'comida',      'generico 3',  'credito 2'],
+     ['05-04-2024', 153167, 320233, 'juegos',      'generico 4',  'credito 2'],
+     ['31-03-2024', 587461, 390925, 'actividades', 'generico 5',  'credito 2'],
+     ['10-04-2024', 357932, 178911, 'actividades', 'generico 6',  'credito 2'],
+     ['31-03-2024', 92780,  374369, 'actividades', 'generico 7',  'credito 2'],
+     ['15-04-2024', 489449, 303474, 'comida',      'generico 8',  'credito 1'],
+     ['29-03-2024', 463201, 185468, 'juegos',      'generico 9',  'credito 1'],
+     ['21-04-2024', 504918, 312476, 'comida',      'generico 10', 'corriente'],
+     ['14-04-2024', 276672, 214862, 'juegos',      'generico 11', 'credito 2'],
+     ['04-04-2024', 71323,  248443, 'juegos',      'generico 12', 'corriente'],
+     ['21-04-2024', 527471, 396922, 'actividades', 'generico 13', 'corriente'],
+];
