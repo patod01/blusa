1963594c2ee0098af44e5fb2454dbabc9448c1cc
patod01 <patocv2@hotmail.com>
2024-06-02 13:23:38 -0400

    Updates add log button
    Now, the form for adding logs gets clean after a new log has been
    added.
    
    `get_shit` function doesn't bug when submitting an empty log.

--- static/js/temp.js
+++ static/js/temp.js
@@ -1,7 +1,8 @@
 let a;
 
 function get_shit({ fecha, abono, cargo, motivo, detalle, fuente }) {
-     fecha = fecha.split('-').reverse().join('-');
+     if (fecha == null) fecha = '';
+     else fecha = fecha.split('-').reverse().join('-');
      if (abono == null) abono = 0;
      if (cargo == null) cargo = 0;
      let new_row = [ fecha, abono, cargo, motivo, detalle, fuente, false ];
