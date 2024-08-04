0465ab239489d5612896402b936c7ec47b924bf8
patod01 <patocv2@hotmail.com>
2024-08-04 16:37:45 -0400

    pa0.6.7 - Acquires Bottle powers
    Python virtual environment added and working through `Bottle`
    framework.
    
    - `.gitignore`: now ignores `.venv`.
    - `app.html`: got deleted after finishing bottle tests.
    - `index.html`: commented two css imports which have not been used.
    - `serv.py`: fully rewritten to support bottle and serve the app and
    every feature developed to the moment.

--- serv.py
+++ serv.py
@@ -1,23 +1,34 @@
-import os, json, time, sqlite3, sys
-from flask import (
-     Flask, request, render_template,
-     url_for, redirect, jsonify,
-)
+import os, sys
+from bottle import error, route, run, static_file, template
 
-app = Flask(__name__)
-
-# Default settings #
-@app.errorhandler(404)
+### Default settings ###
+@error(404)
 def go_default(error):
-     return 'notmyproblem .!.', 404
+     return 'notmyproblem .!.'
 
+@route('/static/<file:path>')
+def static(file):
+     return static_file(file, root='./static')
+
+@route('/info.mjs')
+def info():
+     return static_file('/info.mjs', root='.')
+
+@route('/manifest.json')
+def manifest():
+     return static_file('/manifest.json', root='.')
 
 ### Real sh1t ###
-@app.route('/')
-def home():
-     return render_template('index.html')
+@route('/')
+def hello():
+     return static_file('index.html', root='.')
 
 if __name__ == '__main__':
-     app.run(port=10003)
+     if sys.argv[1] == 'dev':
+          print('Running on', sys.argv[1])
+          run(host='localhost', port=8080, debug=True, reloader=True)
+     if sys.argv[1] == 'FTW':
+          print('Running on', sys.argv[1])
+          run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
 
 #ned
