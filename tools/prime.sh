a6221f69bacb8f38c88cc08ff15be7bb48dedfea
patod01 <patocv2@hotmail.com>
2024-08-12 21:11:06 -0400

    pa0.6.14 - Updates build tools
         renamed:    Dockerfile -> tools/Dockerfile
         new file:   tools/prime.sh
         renamed:    run.sh -> tools/srv.sh
    
    `prime.sh` prepare a simple environment to direct access build tools.

---
+++ tools/prime.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# Run this file from project's root folder!
+
+if [[ $1 = "i" ]]; then
+     python -m venv .venv
+     source .venv/bin/activate
+     pip install --upgrade pip
+     pip install bottle
+     echo Dependencies installed
+     echo Creating bugs folder
+     mkdir app/bugs
+     echo buggin it...
+     cp .venv/lib/python3.12/site-packages/bottle* -r app/bugs
+     echo finished!
+fi
+
+echo pathing...
+
+if [ "$old_PATH" = "" ]; then
+     old_PATH="$PATH"
+     PATH="$(pwd)/tools:$PATH"
+     export old_PATH
+     export PATH
+else
+     echo old_PATH already saved
+fi
+
+killme() {
+     PATH=$old_PATH
+     unset old_PATH
+     export PATH
+     unset -f killme
+     echo some vars cleaned
+}
+
+export -f killme
